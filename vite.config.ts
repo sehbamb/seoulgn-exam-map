388fdb571d79bc7e22fcec0a27ad586a92187ef2
HRDKGN <sehuii.k@gmail.com>
2025-10-23 16:06:48 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// GitHub Pages용 base 설정
 export default defineConfig({
   base: "/seoulgn-exam-map/",
   plugins: [react()]
 });
-
